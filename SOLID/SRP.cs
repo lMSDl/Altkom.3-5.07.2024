b2a0ea16e1d18b1979d4617162a5bd785c5b75e0
Paweł Łęczycki <pawel@msd.info.pl>
2024-07-03 12:56:52 +0200

    SRP

---
+++ SOLID/SRP.cs
@@ -0,0 +1,25 @@
+﻿namespace Altkom._3_5._07._2024.SOLID.S
+{
+    public class User
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+    }
+
+    public class Validators
+    {
+        public bool ValidateEmail(string email)
+        {
+            try
+            {
+                var addr = new System.Net.Mail.MailAddress(email);
+                return addr.Address == email;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
