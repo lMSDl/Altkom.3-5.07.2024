8a0d157876ff632d12bc8e272a0696b3bfb8f671
Paweł Łęczycki <pawel@msd.info.pl>
2024-07-03 10:31:03 +0200

    KISS

---
+++ DesignPrinciples/PaymentService.cs
@@ -0,0 +1,44 @@
+﻿namespace Altkom._3_5._07._2024.DesignPrinciples
+{
+    public class PaymentService
+    {
+        private ICollection<PaymentAccount> PaymentAccounts { get; } = [new PaymentAccount(1), new PaymentAccount(2), new PaymentAccount(3), new PaymentAccount(4), new PaymentAccount(5)];
+
+        public bool DeletePaymentAccount(PaymentAccount account)
+        {
+            return PaymentAccounts.Remove(account);
+        }
+
+        public PaymentAccount? FindByAllowedDebit(float debit)
+        {
+            return PaymentAccounts.SingleOrDefault(x => x.AllowedDebit == debit);
+        }
+
+        public bool Charge(int id, float amount)
+        {
+            var account = PaymentAccounts.SingleOrDefault(x => x.Id == id);
+            if (account == null)
+                return false;
+
+            if (account.Income - account.Outcome + account.AllowedDebit < amount)
+                return false;
+
+            account.Outcome += amount;
+            return true;
+        }
+
+        public void Fund(int id, float amount)
+        {
+            var account = PaymentAccounts.Where(x => x.Id == id).SingleOrDefault();
+            if (account == null)
+                return;
+            account.Income += amount;
+        }
+
+        public float? GetBalance(int id)
+        {
+            var account = PaymentAccounts.Where(x => x.Id == id).SingleOrDefault();
+            return account?.Income - account?.Outcome;
+        }
+    }
+}
