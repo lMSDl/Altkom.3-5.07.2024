057ecf6b9e91d2f1549813d9252fdda7c2db78b4
Paweł Łęczycki <pawel@msd.info.pl>
2024-07-03 15:35:40 +0200

    AbstractFactory: before

---
+++ DesignPatterns/Creational/AbstractFactory/Client.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Altkom._3_5._07._2024.DesignPatterns.Creational.AbstractFactory
+{
+    internal class Client
+    {
+        public static void Execute()
+        {
+            Order order;
+
+            order = new Order("Honda", nameof(ISedan), "compact");
+            Console.WriteLine(order.ManufacturedCarName());
+
+
+            order = new Order("Toyota", nameof(ISedan), "compact");
+            Console.WriteLine(order.ManufacturedCarName());
+
+
+            order = new Order("Toyota", nameof(ISuv), "full");
+            Console.WriteLine(order.ManufacturedCarName());
+
+        }
+    }
+}
---
+++ DesignPatterns/Creational/AbstractFactory/Order.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Altkom._3_5._07._2024.DesignPatterns.Creational.AbstractFactory
+{
+    internal class Order
+    {
+        private readonly ICar _car;
+
+        public Order(string manufacturer, string type, string segment)
+        {
+            switch (manufacturer)
+            {
+                case "Honda":
+                    switch (type)
+                    {
+                        case nameof(ISedan):
+                            switch (segment)
+                            {
+                                case "compact":
+                                    _car = new HondaCompactSedan();
+                                    break;
+                                case "full":
+                                    _car = new HondaFullSedan();
+                                    break;
+                            }
+                            break;
+                        case nameof(ISuv):
+                            switch (segment)
+                            {
+                                case "compact":
+                                    _car = new HondaCompactSuv();
+                                    break;
+                                case "full":
+                                    _car = new HondaFullSuv();
+                                    break;
+                            }
+                            break;
+                    }
+                    break;
+                case "Toyota":
+                    switch (type)
+                    {
+                        case nameof(ISedan):
+                            switch (segment)
+                            {
+                                case "compact":
+                                    _car = new ToyotaCompactSedan();
+                                    break;
+                                case "full":
+                                    _car = new ToyotaFullSedan();
+                                    break;
+                            }
+                            break;
+                        case nameof(ISuv):
+                            switch (segment)
+                            {
+                                case "compact":
+                                    _car = new ToyotaCompactSuv();
+                                    break;
+                                case "full":
+                                    _car = new ToyotaFullSuv();
+                                    break;
+                            }
+                            break;
+                    }
+                    break;
+            }
+        }
+
+        public string ManufacturedCarName()
+        {
+            return _car.Name();
+        }
+
+    }
+}
