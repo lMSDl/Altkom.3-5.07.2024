1cf8fbee83beffb66072a08bce21e782a1ca0b04
Paweł Łęczycki <pawel@msd.info.pl>
2024-07-03 13:49:11 +0200

    DIP

---
+++ SOLID/DIP.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Altkom._3_5._07._2024.SOLID.D
+{
+    interface IMessage
+    {
+        void Send();
+    }
+    class SMS : IMessage
+    {
+        public string Number { get; set; }
+        public string Content { get; set; }
+
+        public void Send()
+        {
+            SendSMS();
+        }
+
+        public void SendSMS()
+        {
+            Console.WriteLine("Sending SMS...");
+        }
+    }
+
+    class MMS : IMessage
+    {
+        public string Number { get; set; }
+        public byte[] Content { get; set; }
+
+        public void Send()
+        {
+            SendMMS();
+        }
+
+        public void SendMMS()
+        {
+            Console.WriteLine("Sending MMS...");
+        }
+    }
+
+    class Mail : IMessage
+    {
+        public string Address { get; set; }
+        public string Subject { get; set; }
+        public string Content { get; set; }
+        public byte[] Attachement { get; set; }
+
+        public void Send()
+        {
+            SendMail();
+        }
+
+        public void SendMail()
+        {
+            Console.WriteLine("Sending Mail...");
+        }
+    }
+
+    class Messenger
+    {
+        public IEnumerable<IMessage> Messages { get; set; }
+
+        public Messenger(IEnumerable<IMessage> messages)
+        {
+            Messages = messages;
+        }
+
+        public void SendMessage(IEnumerable<IMessage> messages)
+        {
+            Messages = messages;
+            SendMessage();
+        }
+
+        public void SendMessage()
+        {
+            foreach (var message in Messages)
+            {
+                message?.Send();
+            }
+        }
+    }
+}
