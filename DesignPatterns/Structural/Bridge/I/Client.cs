bb7ca4f30b1fe67a48a4f44be2e45955c4d4ea53
Paweł Łęczycki <pawel@msd.info.pl>
2024-07-04 10:37:10 +0200

    Bridge: before

---
+++ DesignPatterns/Structural/Bridge/I/Client.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Altkom._3_5._07._2024.DesignPatterns.Structural.Bridge.I
+{
+    internal class Client
+    {
+        public static void Execute()
+        {
+            Shape shape = new Square();
+            Console.WriteLine(shape);
+
+            Shape circle = new Circle();
+            Console.WriteLine(circle);
+        }
+    }
+}
---
+++ DesignPatterns/Structural/Bridge/I/Square.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Altkom._3_5._07._2024.DesignPatterns.Structural.Bridge.I
+{
+    internal class Square : Shape
+    {
+    }
+}
