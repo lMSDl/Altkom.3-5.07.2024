5e33427f3e0d7be850b32076ed7e6e2e4e15668a
Paweł Łęczycki <pawel@msd.info.pl>
2024-07-04 09:58:08 +0200

    ObjectPool: after

---
+++ DesignPatterns/Creational/ObjectPool/ItemPool.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Altkom._3_5._07._2024.DesignPatterns.Creational.ObjectPool
+{
+    internal class ItemPool
+    {
+        private IReadOnlyCollection<Item> _items;
+        public ItemPool(int count)
+        {
+            _items = Enumerable.Range(1, count).Select(x => new Item()).ToList();
+        }
+
+        public Item? Acquire()
+        {
+            lock (this)
+            {
+                var item = _items.FirstOrDefault(x => !x.IsVisible);
+                if (item is not null)
+                    item.IsVisible = true;
+                return item;
+            }
+        }
+    }
+}
---
+++ DesignPatterns/Creational/ObjectPool/ObjectPool.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Altkom._3_5._07._2024.DesignPatterns.Creational.ObjectPool
+{
+    internal class ObjectPool<T>
+    {
+        private ConcurrentQueue<T> _queue;
+        public ObjectPool(int count, Func<T> create)
+        {
+            _queue = new ConcurrentQueue<T>(Enumerable.Range(1, count).Select(x => create()).ToList());
+        }
+
+        public T? Acquire()
+        {
+            if (_queue.TryDequeue(out var item))
+                return item;
+            return default;
+        }
+
+        public void Release(T item)
+        {
+            _queue.Enqueue(item);
+        }
+    }
+}
