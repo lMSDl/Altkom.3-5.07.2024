5057eb7b89b907b0dca4cef91350e84ba9a11235
Paweł Łęczycki <pawel@msd.info.pl>
2024-07-04 12:18:53 +0200

    Flyweight: before

---
+++ DesignPatterns/Structural/Flyweight/Client.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Altkom._3_5._07._2024.DesignPatterns.Structural.Flyweight
+{
+    internal class Client
+    {
+        public static void Execute()
+        {
+            var product1 = new Product
+            {
+                Name = "N1",
+                Description = "D1",
+                ExpirationDate = DateTime.Now.AddDays(30),
+                ProductionDate = DateTime.Now,
+                Logo = new byte[1024],
+                Weight = 100,
+                Manufacturer = "M1"
+            };
+
+            var product2 = new Product
+            {
+                Name = "N2",
+                Description = "D2",
+                ExpirationDate = DateTime.Now.AddDays(30),
+                ProductionDate = DateTime.Now,
+                Logo = new byte[1024],
+                Weight = 100,
+                Manufacturer = "M1"
+            };
+
+            Show(product1);
+            Show(product2);
+
+            product1.ShowShortInfo();
+        }
+
+        public static void Show(Product product)
+        {
+            Console.WriteLine($"{product.Name}; {product.Description}; {product.Manufacturer}; {product.ProductionDate.ToShortDateString()}; {product.ExpirationDate.ToShortDateString()}");
+        }
+    }
+}
---
+++ DesignPatterns/Structural/Flyweight/Product.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Altkom._3_5._07._2024.DesignPatterns.Structural.Flyweight
+{
+    internal class Product
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int Weight { get; set; }
+        public string Manufacturer { get; set; }
+        public byte[] Logo { get; set; }
+        public DateTime ProductionDate { get; set; }
+        public DateTime ExpirationDate { get; set; }
+
+        public void ShowShortInfo()
+        {
+            Console.WriteLine($"{Name} ({ExpirationDate.ToShortDateString()})");
+        }
+    }
+}
