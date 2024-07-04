ce892b562817583f8d62845be8511c611a08b9ae
Paweł Łęczycki <pawel@msd.info.pl>
2024-07-04 09:44:34 +0200

    ObjectPool: before

---
+++ DesignPatterns/Creational/ObjectPool/Item.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Altkom._3_5._07._2024.DesignPatterns.Creational.ObjectPool
+{
+    internal class Item
+    {
+        private bool isCollected;
+
+        public bool IsVisible { get; set; }
+        public bool IsCollected
+        {
+            get => isCollected;
+            set
+            {
+                if (isCollected = value)
+                    IsVisible = false;
+            }
+        }
+
+        public byte[] Content { get; } = new byte[1024];
+    }
+}
